953d2c5f177c4093b57f1d6810ae551916de5dd4
Slangeman <fredrik.langa@gmail.com>
2019-03-19 15:25:54 +0100

    now adding issues in real-time

--- public/client.js
+++ public/client.js
@@ -11,4 +11,24 @@ socket.on('newComment', issue => {
 socket.on('newIssue', issue => {
   console.log(issue)
   console.log('new issue added')
+  let issueContainer = document.querySelector('.issue-container')
+  let issues = document.querySelector('.issues')
+  let issueClone = document.importNode(issues, true)
+  let id = document.querySelector('.issue-id')
+  let title = document.querySelector('.issue-title')
+  let body = document.querySelector('.issue-body')
+  let state = document.querySelector('.issue-state')
+  let comments = document.querySelector('.issue-comments')
+  let url = document.querySelector('.issue-url')
+
+  id.setAttribute('id', `issue-${issue.id}`)
+  title.textContent = issue.title
+  body.textContent = issue.body
+  state.textContent = `Issue state: ${issue.state}`
+  comments.textContent = `Comments: ${issue.comments}`
+  url.setAttribute('href', `https://github.com/${issue.url}`)
+
+  issueContainer.insertBefore(issueClone, issueContainer)
+
+  console.log(issueClone)
 })
